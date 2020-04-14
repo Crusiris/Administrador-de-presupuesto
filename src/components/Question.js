1f6d371ed21a7f38459d5dd401efc400fa34a6de
crusiris <cruzsirys@gmail.com>
2020-04-14 18:27:43 -0400

    Creando espacio de trabajo y el componente Question

---
+++ src/components/Question.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+
+const Question = () => {
+    return (  
+        <Fragment>
+            <h1>Ingresa tu presupuesto</h1>
+        </Fragment>
+    );
+}
+ 
+export default Question;
