99f75d73109ee7cade9a3e404e9c3d30c352c393
crusiris <cruzsirys@gmail.com>
2020-04-15 17:19:00 -0400

    Creando el formulario de gastos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import Question from './components/Question'
-
+import Formulario from './components/Formulario'
 
 function App() {
 //State del presupuesto
@@ -10,14 +10,26 @@ const [restBudget, saveRest]= useState(0)
 
   return (
     <div className="container">
-      <h1>Gasto semanal</h1>
-      <div className="contenido-principal contenido">
-      <Question
-      saveBudget= { saveBudget }
-      saveRest= { saveRest }
-      />
-      </div>
-      
+      <header>
+          <h1>Gasto semanal</h1>
+
+            <div className="contenido-principal contenido">
+              <Question
+              saveBudget= {saveBudget}
+              saveRest= {saveRest}
+              />
+
+            <div className="row">
+                <div className="one-half column">
+                  <Formulario/>
+                </div>
+                <div className="one-half column">
+                  2
+                </div>  
+            </div>
+
+            </div>
+            </header>
     </div>
   );
 }
